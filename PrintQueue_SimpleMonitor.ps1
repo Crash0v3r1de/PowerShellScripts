1d3dc4bf0a97878a526664fb9f85e261b9b4ada9
Crash0v3r1de <none@gmail.com>
2019-11-07 13:38:53 -0700

    add windows notification in script, fixed queue validation issue

--- PrintQueue_SimpleMonitor.ps1
+++ PrintQueue_SimpleMonitor.ps1
@@ -23,8 +23,21 @@ return -1; # Handle reported error?
 }
 }
 }
+function SendCloggedNotice{
+# Send notice so you can see it
+Add-Type -AssemblyName System.Windows.Forms 
+$global:balloon = New-Object System.Windows.Forms.NotifyIcon
+$path = (Get-Process -id $pid).Path
+$balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path) 
+$balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning 
+$balloon.BalloonTipText = 'The print queue for '+$Printer+' may be clogged.
+Please check the queue manually!'
+$balloon.BalloonTipTitle = "Print Queue Monitor Alert" 
+$balloon.Visible = $true 
+$balloon.ShowBalloonTip(7000) # Show for 7 seconds i guess
+}
 
-
+$temp = GetJobs;
 # This is is basically
-if (GetJobs -gt 3){Write-Output "Many print jobs waiting in queue.....please check";}
+if ($temp -gt 3){Write-Output "Many print jobs waiting in queue.....please check";SendCloggedNotice;}
 else{Write-Output "Print queue looks normal, going to sleep...";}
