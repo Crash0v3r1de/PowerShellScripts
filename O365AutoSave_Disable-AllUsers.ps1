710343c3ea3e275a2d9f994bf20cc873c47001a8
Crash0v3r1de <nb.faulds@gmail.com>
2020-01-24 15:04:23 -0700

    per user and all user cloud autosave disable scripts added

---
+++ O365AutoSave_Disable-AllUsers.ps1
@@ -0,0 +1,47 @@
+﻿#######################################################################################
+# Script Author - Nate Faulds <nb.faulds@gmail.com>  with help of PDQ's Kris Powell   #
+# Purpose - I have customers who use PGP and their documents will always fail to      #
+#    autosave to Office365/Sharepoint since the files are not a readable format.      #
+#  This script automaticaly disables autosave for all users to O365 and SharePoint    #
+#             This DOES NOT effect AutoRecovery and local AutoSave                    #
+#        Side note: PDQ Deploy is a fantastic piece of software for admins            #
+#######################################################################################
+
+function ProcessAllUsers{
+####################################################################
+# This function code snippet was pulled from                       #
+# https://www.pdq.com/blog/modifying-the-registry-users-powershell #
+#  This is a fantastic snippet so no reason to write from scratch  #
+####################################################################
+
+$PatternSID = 'S-1-5-21-\d+-\d+\-\d+\-\d+$'
+$ProfileList = gp 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*' | Where-Object {$_.PSChildName -match $PatternSID} | 
+    Select  @{name="SID";expression={$_.PSChildName}}, 
+            @{name="UserHive";expression={"$($_.ProfileImagePath)\ntuser.dat"}}, 
+            @{name="Username";expression={$_.ProfileImagePath -replace '^(.*[\\\/])', ''}}
+$LoadedHives = gci Registry::HKEY_USERS | ? {$_.PSChildname -match $PatternSID} | Select @{name="SID";expression={$_.PSChildName}}
+$UnloadedHives = Compare-Object $ProfileList.SID $LoadedHives.SID | Select @{name="SID";expression={$_.InputObject}}, UserHive, Username
+Foreach ($item in $ProfileList) {
+    IF ($item.SID -in $UnloadedHives.SID) {
+        reg load HKU\$($Item.SID) $($Item.UserHive) | Out-Null
+    }
+    $wordFound = CheckOfficePresent $Item.SID # If Word is missing nothing else will be there
+    if($wordFound){
+    Set-ItemProperty registry::HKEY_USERS\$($Item.SID)\Software\Microsoft\Office\16.0\word -Name AutoSaveByDefaultUserChoice -Value 2
+    Set-ItemProperty registry::HKEY_USERS\$($Item.SID)\Software\Microsoft\Office\16.0\excel -Name AutoSaveByDefaultUserChoice -Value 2
+    Set-ItemProperty registry::HKEY_USERS\$($Item.SID)\Software\Microsoft\Office\16.0\powerpoint -Name AutoSaveByDefaultUserChoice -Value 2
+    }      
+    IF ($item.SID -in $UnloadedHives.SID) {
+        [gc]::Collect()
+        reg unload HKU\$($Item.SID) | Out-Null
+    }
+}
+
+}
+function CheckOfficePresent([string]$regPath){
+$found = Get-ItemProperty -Path registry::HKEY_USERS\$regPath\Software\Microsoft\Office\16.0\word -Name WordName
+if($found){ return 1}
+else{ return 0}  # location is null/missing
+}
+
+ProcessAllUsers
---
+++ O365AutoSave_Disable-PerUser.ps1
@@ -0,0 +1,80 @@
+﻿#######################################################################################
+# Author - Nate Faulds <nb.faulds@gmail.com>                                          #
+# Purpose - I have customers who use PGP and their documents will always fail to      #
+#    autosave to Office365/Sharepoint since the files are not a readable format.      #
+#  This script automaticaly disables autosave for current user to O365 and SharePoint #
+#             This DOES NOT effect AutoRecovery and local AutoSave                    #
+#######################################################################################
+
+# Check if we're debugging 
+param([bool]$debug=0) # assign 1 for true to enable debug or leave blank for 0 to disable debug
+
+# Global Declerations
+$path = "HKCU:\SOFTWARE\Microsoft\Office\16.0"
+# End Global Declerations
+
+# Check for registry item
+# I REALLLYYYYYY hate the fact that try catch has no effect with this type of checking
+function WordRegEntry{
+$wordPresent = Get-ItemProperty -Path $path\Word -Name AutoSaveByDefaultUserChoice
+if($wordPresent){return 1}  # Returns that word is set for no autosave - true
+else{return 0}   # Returns that word is NOT set to disable autosave (it's null) - false
+}
+function ExcelRegEntry{
+$excelPresent = Get-ItemProperty -Path $path\Excel -Name AutoSaveByDefaultUserChoice
+if($excelPresent){return 1}
+else{return 0}
+}
+function PowerpointRegEntry{
+$powerpointPresent = Get-ItemProperty -Path $path\Powerpoint -Name AutoSaveByDefaultUserChoice
+if($powerpointPresent){return 1}
+else{return 0}
+}
+
+# Set the registry settings
+function SetWord{
+Set-ItemProperty -Path $path\Word -Name AutoSaveByDefaultUserChoice -Value 2
+$wordSet = Get-ItemProperty -Path $path\Word -Name AutoSaveByDefaultUserChoice
+if($wordSet){return 1}
+else{ 
+Write-Host 'Error occured setting Word autosave entry'
+return 0}
+}
+function SetExcel{
+Set-ItemProperty -Path $path\Excel -Name AutoSaveByDefaultUserChoice -Value 2
+$excelSet = Get-ItemProperty -Path $path\Excel -Name AutoSaveByDefaultUserChoice
+if($excelSet){return 1}
+else{ 
+Write-Host 'Error occured setting Excel autosave entry'
+return 0}
+}
+function SetPowerpoint{
+Set-ItemProperty -Path $path\Powerpoint -Name AutoSaveByDefaultUserChoice -Value 2
+$powerpointSet = Get-ItemProperty -Path $path\Powerpoint -Name AutoSaveByDefaultUserChoice
+if($powerpointSet){return 1}
+else{ 
+Write-Host 'Error occured setting Powerpoint autosave entry'
+return 0}
+}
+
+# Closest thing to C# i can get i guess ¯\_(ツ)_/¯
+function Main{
+if($debug){ForceReset}
+
+$wordFound = WordRegEntry
+$excelFound = ExcelRegEntry
+$powerpointFound = PowerpointRegEntry
+
+if(!$wordFound){ SetWord } # False for setting being present
+if(!$excelFound){ SetExcel }
+if(!$powerpointFound){ SetPowerpoint }
+}
+function ForceReset{
+Remove-ItemProperty -Path $path\Word -Name AutoSaveByDefaultUserChoice
+Remove-ItemProperty -Path $path\Excel -Name AutoSaveByDefaultUserChoice
+Remove-ItemProperty -Path $path\Powerpoint -Name AutoSaveByDefaultUserChoice
+}
+
+Write-Host $PSCommandPath
+
+Main
